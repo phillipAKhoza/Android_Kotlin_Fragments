5c6311efe1bbb872e754f2d146af8ff3ade2e1d7
phillip khoza <phillipantonk@gmail.com>
2024-02-02 15:02:41 +0200

    project init

---
+++ app/src/main/java/com/phillipdev/listfragment/MyListFragment.kt
@@ -0,0 +1,22 @@
+package com.phillipdev.listfragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.ListFragment
+
+class MyListFragment : ListFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_list, container, false)
+    }
+
+}
