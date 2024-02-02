4a5262e9289621843ae28427ffc1698811bc257c
phillip khoza <phillipantonk@gmail.com>
2024-02-02 12:24:02 +0200

    Replacing fragments (replacing first fragment with second fragment using a button)

--- app/src/main/java/com/phillipdev/fragments/MainActivity.kt
+++ app/src/main/java/com/phillipdev/fragments/MainActivity.kt
@@ -2,13 +2,18 @@ package com.phillipdev.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var nxtFragment : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nxtFragment = findViewById(R.id.btnReplace)
+
 
         val fragmentManager : FragmentManager = supportFragmentManager
         val fragmentTransaction : FragmentTransaction = fragmentManager.beginTransaction()
@@ -18,5 +23,16 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.add(R.id.frame, firstFragment)
 
         fragmentTransaction.commit()
+
+
+        nxtFragment.setOnClickListener {
+            val secondFragmentManager: FragmentManager = supportFragmentManager
+            val secondFragmentTraction : FragmentTransaction = secondFragmentManager.beginTransaction()
+
+            val secondFragment = SecondFragment()
+
+            secondFragmentTraction.replace(R.id.frame, secondFragment)
+            secondFragmentTraction.commit()
+        }
     }
 }
